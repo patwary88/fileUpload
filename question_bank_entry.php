07d7d09d00ec0c332d2d691f3a800b9f6796cabd
patwary88 <mainuddinpatwary1988@gmail.com>
2020-02-02 23:37:23 +0600

    myproject

---
+++ question_bank_entry.php
@@ -0,0 +1,36 @@
+<?php
+    require_once('includes/common.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>File Upload</title>
+
+<link rel="stylesheet" type="text/css" href="resources/bootstrap.min.css">
+<script type="text/javascript" src="resources/jquery.min.js"></script>
+<script type="text/javascript" src="resources/popper.min.js"></script>
+<script type="text/javascript" src="resources/bootstrap.min.js"></script>
+<script type="text/javascript"></script>
+</head>
+<body>
+	<div class="container">
+	  <h2 style="text-align: center;">Question Bnak Entry</h2>
+	  <form>
+	    <div class="row">
+	      <div class="col-sm-8">
+	        <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+	      </div>
+	      <div class="col-sm-2">
+	        <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+	      </div>
+	      <div class="col-sm-2">
+	        <input type="text" class="form-control" id="email" placeholder="Enter email" name="email">
+	      </div>
+	    </div>
+	  </form>
+	</div>
+</body>
+</html>
